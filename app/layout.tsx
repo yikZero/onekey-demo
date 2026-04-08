cfb9badc520fd692fd2c415aac700c8672ceba92
yikZero <i@yikzero.com>
2026-04-08 11:22:55 +0800

    chore: init from create-rotor

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Geist_Mono } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: 'my-app',
+  description: '',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  )
+}
